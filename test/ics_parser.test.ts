749865ff4193024b40679465de0735972b02eb16
Rodrigo Martins Mansueli <rodrigo@mansueli.com>
2023-08-01 22:24:23 -0300

    Update ics_parser.test.ts

--- test/ics_parser.test.ts
+++ test/ics_parser.test.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import parseICS from './ics_parser.ts';
+import parseICS from '../ics_parser.ts';
 
 describe('parseICS', () => {
   let icsString: string;
